e62a998a01d73264f3a9e5019d730eb08d444218
Ref-Bit <rsv.gho@gmail.com>
2020-12-20 14:41:08 +0300

    Initialized Redux Store with File upload input

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { setUploadFile } from './redux/uploadFile/uploadFile.actions';
 
-function App() {
+function App(props) {
+  const handleAttachFile = e => {
+    // Chance of validation here...
+    console.log('File Uploads', e.target.files);
+    props.setUploadFile(e.target.files);
+    e.target.value = ''; // Clear the current file
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center text-6xl text-gray-700 my-8">Progress Act</h1>
+      {/* <label>Upload Files</label>
+      <input type="file" onChange={handleAttachFile} multiple /> */}
+
+      <div className="flex w-full items-center justify-center bg-grey-lighter">
+        <label className="w-64 flex flex-col items-center px-4 py-6 hover:bg-white hover:text-gray-700 rounded-lg shadow-lg tracking-wide uppercase border border-gray-100 cursor-pointer text-gray-100 bg-gray-700 transition duration-300">
+          <svg
+            className="w-8 h-8"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+          </svg>
+          <span className="mt-2 text-base leading-normal">Select a file</span>
+          <input type="file" className="hidden" onChange={handleAttachFile} multiple/>
+        </label>
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setUploadFile: files => dispatch(setUploadFile(files)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
